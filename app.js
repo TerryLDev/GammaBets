52c3d0b5c0651f0e72fd81d5441bc9aeb0c2bb3c
Terry <tergeek@gmail.com>
2021-09-28 00:57:26 -0400

    Testing functions

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const Koa = require('koa');
+const KoaRouter = require('koa-router');
 const json = require('koa-json');
+// Need a template engine still
+
 const app = new Koa();
+const router = new KoaRouter();
 
 app.use(json());
 
-app.use(async ctx => ctx.body = 'Hello');
+// Simple Middleware
+// app.use(async ctx => ctx.body = 'Hello');
+
+router.get('/test', ctx => (ctx.body = 'Hello Test'));
+
+// Router
+app.use(router.routes()).use(router.allowedMethods)
 
 app.listen(3000, () => console.log('Server Started...'));
